cb39ee7591cdbff10b1276de779316c07f4a8b52
ghshin <leteu610@gmail.com>
2024-07-16 15:25:19 +0900

    Fix: console easter egg font bug fix

--- .vitepress/theme/consoleText.ts
+++ .vitepress/theme/consoleText.ts
@@ -1,5 +1,13 @@
 const css = `
-    font-family: 'MesloLGS NF';
+    @font-face {
+      font-family: 'MesloLGS NF';
+      font-weight: normal;
+      font-style: normal;
+      font-display: swap;
+      src: url('/fonts/MesloLGSNF/MesloLGS-NF-Regular.woff2') format('woff2');
+    }
+
+    font-family: 'MesloLGS NF', 'Courier New', monospace;
     background: linear-gradient(90deg,#00ffb3 0%, #00a372 60%, #0080a3 100%);
     background-clip: text;
     -webkit-text-fill-color: transparent;
